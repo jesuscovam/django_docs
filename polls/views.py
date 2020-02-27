5ec1780356554bdbbacc5f069203f8815b7e115b
jesuscova <jesuscovam@icloud.com>
2020-02-27 10:44:29 -0500

    added test to avoid rendering of question with future dates

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from django.urls import reverse
 from django.views import generic
 
@@ -12,8 +13,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published question"""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published question
+        (not including those set to be published in the future)
+        """
+        return Question.objects.filter(
+            pub_date__lte= timezone.now()
+        ).order_by('-pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -36,3 +42,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
